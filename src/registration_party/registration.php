89ca1f83ff9f7c2edd2ac6b5790e0f9fde82f448
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2021-07-15 18:59:34 +0530

    Toggle between light and dark mode dynamically

--- src/registration_party/registration.php
+++ src/registration_party/registration.php
@@ -4,7 +4,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="../css/virtualelection.css">
+    <script src="../controllers/css.js"></script>
+    <script>
+        loadCSS(1);
+    </script>
     <title>Party Registration</title>
 </head>
 
