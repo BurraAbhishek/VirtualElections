44154fbd363a94599fac8d9bc9064e4fd9d70bdd
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2021-08-07 17:52:57 +0530

    Use public domain for ssl.php

--- src/db/controllers/ssl.php
+++ src/db/controllers/ssl.php
@@ -1,4 +1,26 @@
 <?php
+	/**
+	 * OpenSSL encryption and decryption class.
+	 * 
+	 * This connection class has 4 properties:
+	 * <ul>
+	 * 		<li> $method: The encryption/decryption algorithm used. </li>
+	 * 		<li> $passphrase: The passphrase. </li>
+	 * 		<li> $options: Bitwise disjunction of flags OPENSSL_RAW_DATA and OPENSSL_ZERO_PADDING </li>
+	 * 		<li> $iv: Refers to the initialization vector. Should not be null. </li>
+	 * </ul>
+	 * NOTE:
+	 * <ul>
+	 * 		<li> Please change the passphrase and initialization vector as soon as possible. </li>
+	 * 		<li> Do not change any of these values once the application is deployed </li>
+	 * </ul>
+     * 
+	 * This class can be used in any application.
+	 *
+	 * @author Burra Abhishek
+	 * @license Public Domain
+	 * 
+	 */
     class SecureData
     {
         private $method = "AES-256-CBC-HMAC-SHA256";
