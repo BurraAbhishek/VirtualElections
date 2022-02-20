ef9fd32b6498015e3d26d7cd3bb27215b3c736ce
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2022-02-20 23:33:45 +0530

    provision: Generate varchar primary keys
    Instead of auto-incrementing integers, use shrug strings as primary keys.

--- src/registration_party/controllers/party_register.php
+++ src/registration_party/controllers/party_register.php
@@ -5,6 +5,8 @@
 	require_once '../../db/config/tablesconfig.php';
 	require_once '../../db/controllers/ssl.php';
 	require_once '../../db/controllers/post_validate.php';
+	require_once '../../db/config/idconfig.php';
+	require_once '../../db/config/shrugconfig.php';
 
 	try {
 		$dbconn = new Connection();
@@ -16,31 +18,39 @@
 	$tables = new Table();
 	$party = $tables->getPartyList();
 	$party_table = $party["table"];
+	$party_id = $party["id"];
 	$party_name = $party["party_name"];
 	$candidate = $party["candidate_name"];
 	$idno = $party["idproof_value"];
 	$idproof = $party["idproof_type"];
 	$crypto = new SecureData();
+	$id_validator = new IDProof();
 
 	try {
 		if(isset($_POST['save'])){
+			$cid = validateDatatype(generate_id(8), 'string');
 			$pname = $crypto->encrypt(
 				validateName(validateDatatype($_POST['pname'], 'string'))
 			);
 			$cname = $crypto->encrypt(validateName(
 				validateDatatype($_POST['cname'], 'string')
 			));
-			$citype =  $crypto->encrypt(validateName(
-				validateDatatype($_POST['citype'], 'string')
-			));
+			$citype_unsafe = validateDatatype($_POST['citype'], 'string');
+				if (!$id_validator->validate_idproof($citype_unsafe)) {
+					throw new Exception("Invalid Identity proof");
+				}
+				$citype = $crypto->encrypt(validateName(
+					$citype_unsafe)
+				);
 			$cidno =  $crypto->encrypt(validateID(
 				validateDatatype($_POST['cidno'], 'string')
 			));
 			$sql = $conn->prepare(
 				"INSERT INTO 
-				$party_table ($party_name, $candidate, $idno, $idproof) 
-				values (:pname,:cname,:cidno, :citype)"
+				$party_table ($party_id, $party_name, $candidate, $idno, $idproof) 
+				values (:cid,:pname,:cname,:cidno, :citype)"
 			);
+			$sql->bindParam(':cid', $cid, PDO::PARAM_STR, 8);
 			$sql->bindParam(':pname', $pname, PDO::PARAM_STR, 512);
 			$sql->bindParam(':cname', $cname, PDO::PARAM_STR, 512);
 			$sql->bindParam(':citype', $citype, PDO::PARAM_STR, 256);
